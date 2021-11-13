9c8f5f72e962a16266e6e00dba732eea7f2128bc
Djamel Ramdani <enzonpapa@gmail.com>
2021-11-13 17:30:18 -0600

    re design some css and set variables for js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  var initialTime = 75;
+  var time = 75;
+  var score = 0;
+  var Count = 0;
+  var timeset;
+  var answers = document.querySelectorAll("#Quiz-hold button");
+
+ 
